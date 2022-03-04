6e51c1ac9b2fca9364bb444590ac887cedce6714
symaeng98 <symaeng98@naver.com>
2022-03-04 18:50:08 +0900

    드래그 앤 드롭 구현 + 로컬에 파일 저장

--- src/main/java/com/example/sudabangweb/controller/MainController.java
+++ src/main/java/com/example/sudabangweb/controller/MainController.java
@@ -2,19 +2,58 @@ package com.example.sudabangweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
+
     @GetMapping("/main")
-    public String home(Model model){
-        model.addAttribute("username","맹순영"); //attributeValue에 DB에서 받아온 값 넣어줘야됨
+    public String dragAndDrop() {
         return "mainPage";
     }
 
-    @GetMapping("/woi")
-    public String woi(Model model){
-        model.addAttribute("woi","wiwwiw");
-        return "woi";
+    @RequestMapping(value = "/main/post") //ajax에서 호출하는 부분
+    @ResponseBody
+    public String upload(MultipartHttpServletRequest multipartRequest) { //Multipart로 받는다.
+
+        Iterator<String> itr =  multipartRequest.getFileNames();
+
+        String filePath = "C:/Users/home/Desktop/SudabangWeb/src/main/resources/static/excel";
+
+        while (itr.hasNext()) { //받은 파일들을 모두 돌린다.
+
+            /* 기존 주석처리
+            MultipartFile mpf = multipartRequest.getFile(itr.next());
+            String originFileName = mpf.getOriginalFilename();
+            System.out.println("FILE_INFO: "+originFileName); //받은 파일 리스트 출력'
+            */
+
+            MultipartFile mpf = multipartRequest.getFile(itr.next());
+
+            String originalFilename = mpf.getOriginalFilename(); //파일명
+
+            String fileFullPath = filePath+"/"+originalFilename; //파일 전체 경로
+
+            try {
+                //파일 저장
+                mpf.transferTo(new File(fileFullPath));
+
+                System.out.println("originalFilename => "+originalFilename);
+                System.out.println("fileFullPath => "+fileFullPath);
+
+            } catch (Exception e) {
+                System.out.println("postTempFile_ERROR======>"+fileFullPath);
+                e.printStackTrace();
+            }
+
+        }
+
+        return "success";
     }
 }
--- src/main/resources/static/js/dragAndDrop.js
+++ src/main/resources/static/js/dragAndDrop.js
@@ -1,47 +1,134 @@
-var sec9 = document.querySelector('#ex9');
-var btnUpload = sec9.querySelector('.btn-upload');
-var inputFile = sec9.querySelector('input[type="file"]');
-var uploadBox = sec9.querySelector('.upload-box');
+$(document).ready(function(){
+    var objDragAndDrop = $(".dragAndDropDiv");
 
-/* 박스 안에 Drag 들어왔을 때 */
-uploadBox.addEventListener('dragenter', function(e) {
-    console.log('dragenter');
-});
+    objDragAndDrop.on("dragenter",function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        $(this).css('background-color','#E3F2FC');
+    });
+    objDragAndDrop.on("dragleave",function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        $(this).css('background-color','#FFFFFF');
+    });
+    objDragAndDrop.on("dragover",function(e){
+        e.stopPropagation();
+        e.preventDefault();
+    });
+    objDragAndDrop.on("drop",function(e){
+        $(this).css('background-color','#FFFFFF');
+        e.preventDefault();
+        var files = e.originalEvent.dataTransfer.files;
 
-/* 박스 안에 Drag를 하고 있을 때 */
-uploadBox.addEventListener('dragover', function(e) {
-    e.preventDefault();
+        handleFileUpload(files,objDragAndDrop);
+    });
 
-    var valid = e.dataTransfer.types.indexOf('Files') >= 0;
+    //drag 영역 클릭시 파일 선택창
+    objDragAndDrop.on('click',function (e){
+        $('input[type=file]').trigger('click');
+    });
 
-    if(!valid){
-        this.style.backgroundColor = 'red';
+    $('input[type=file]').on('change', function(e) {
+        var files = e.originalEvent.target.files;
+        handleFileUpload(files,objDragAndDrop);
+    });
+
+    function handleFileUpload(files,obj)
+    {
+        for (var i = 0; i < files.length; i++)
+        {
+            var fd = new FormData();
+            fd.append('file', files[i]);
+
+            var status = new createStatusbar(obj); //Using this we can set progress.
+            status.setFileNameSize(files[i].name,files[i].size);
+            sendFileToServer(fd,status);
+
+        }
     }
-    else {
-        this.style.backgroundColor = 'green';
+
+    var rowCount=0;
+    function createStatusbar(obj){
+
+        rowCount++;
+        var row="odd";
+        if(rowCount %2 ==0) row ="even";
+        this.statusbar = $("<div class='statusbar "+row+"'></div>");
+        this.filename = $("<div class='filename'></div>").appendTo(this.statusbar);
+        this.size = $("<div class='filesize'></div>").appendTo(this.statusbar);
+        this.progressBar = $("<div class='progressBar'><div></div></div>").appendTo(this.statusbar);
+        this.abort = $("<div class='abort'>중지</div>").appendTo(this.statusbar);
+
+        obj.after(this.statusbar);
+
+        this.setFileNameSize = function(name,size){
+            var sizeStr="";
+            var sizeKB = size/1024;
+            if(parseInt(sizeKB) > 1024){
+                var sizeMB = sizeKB/1024;
+                sizeStr = sizeMB.toFixed(2)+" MB";
+            }else{
+                sizeStr = sizeKB.toFixed(2)+" KB";
+            }
+
+            this.filename.html(name);
+            this.size.html(sizeStr);
+        }
+
+        this.setProgress = function(progress){
+            var progressBarWidth =progress*this.progressBar.width()/ 100;
+            this.progressBar.find('div').animate({ width: progressBarWidth }, 10).html(progress + "% ");
+            if(parseInt(progress) >= 100)
+            {
+                this.abort.hide();
+            }
+        }
+
+        this.setAbort = function(jqxhr){
+            var sb = this.statusbar;
+            this.abort.click(function()
+            {
+                jqxhr.abort();
+                sb.hide();
+            });
+        }
     }
 
-    console.log('dragover');
+    function sendFileToServer(formData,status)
+    {
+        var uploadURL = "/main/post"; //Upload URL
+        var extraData ={}; //Extra Data.
+        var jqXHR=$.ajax({
+            xhr: function() {
+                var xhrobj = $.ajaxSettings.xhr();
+                if (xhrobj.upload) {
+                    xhrobj.upload.addEventListener('progress', function(event) {
+                        var percent = 0;
+                        var position = event.loaded || event.position;
+                        var total = event.total;
+                        if (event.lengthComputable) {
+                            percent = Math.ceil(position / total * 100);
+                        }
+                        //Set progress
+                        status.setProgress(percent);
+                    }, false);
+                }
+                return xhrobj;
+            },
+            url: uploadURL,
+            type: "POST",
+            contentType:false,
+            processData: false,
+            cache: false,
+            data: formData,
+            success: function(data){
+                status.setProgress(100);
 
-    this.style.backgroundColor = 'green';
-});
+                //$("#status1").append("File upload Done<br>");
+            }
+        });
 
-/* 박스 밖으로 Drag가 나갈 때 */
-uploadBox.addEventListener('dragleave', function(e) {
-    console.log('dragleave');
+        status.setAbort(jqXHR);
+    }
 
-    this.style.backgroundColor = 'white';
-});
-
-/* 박스 안에서 Drag를 Drop했을 때 */
-uploadBox.addEventListener('drop', function(e) {
-    e.preventDefault();
-
-    console.log('drop');
-    this.style.backgroundColor = 'white';
-
-    console.dir(e.dataTransfer);
-
-    var data = e.dataTransfer.files[0];
-    console.dir(data);
 });
