904973332e9338d0924e4231e3c34890b8ff961f
qwer55252 <qwer55252@gmail.com>
2022-03-11 00:25:38 +0900

    WeekData<OneDayData>로 수정

--- src/main/java/com/example/sudabangweb/dto/WeekData.java
+++ src/main/java/com/example/sudabangweb/dto/WeekData.java
@@ -2,33 +2,26 @@ package com.example.sudabangweb.dto;
 import java.util.*;
 
 public class WeekData { // 같은 주차의 데이터들만 존재
-    private ArrayList<StudentDTO> weekData;
-    private String date;
+    private ArrayList<OneDayData> weekData;
     private String week;
 
-    public String getDate() {
-        return date;
-    }
-    public void setDate(String date) {
-        this.date = date;
-    }
     public String getWeek() {
         return week;
     }
     public void setWeek(String week) {
         this.week = week;
     }
-    public ArrayList<StudentDTO> getWeekData() {
+    public ArrayList<OneDayData> getWeekData() {
         return weekData;
     }
-    public void setWeekData(ArrayList<StudentDTO> weekData) {
+    public void setWeekData(ArrayList<OneDayData> weekData) {
         this.weekData = weekData;
     }
 
     public WeekData() {
 
     }
-    public void addData(StudentDTO studentData) {
-        weekData.add(studentData);
+    public void addData(OneDayData oneDayData) {
+        weekData.add(oneDayData);
     }
 }
